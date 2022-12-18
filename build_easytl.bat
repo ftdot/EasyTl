1e9ce50d0b8bec6a112d326c87070692f341f035
policere <59689715+policere@users.noreply.github.com>
2022-12-18 20:58:58 +0200

    Directories reformat

--- build_easytl.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-
-set PYTHONPATH=%cd%
-
-:: compile the EasyTl
-python build/compile_easytl_gui.py
-python build/compile_easytl_core.py easytl-build/bin
-
-exit
--- build_easytl.sh
+++
@@ -1,5 +0,0 @@
-export PYTHONPATH=%cd%
-
-# compile the EasyTl
-python3 build/compile_easytl_gui.py
-python3 build/compile_easytl_core.py easytl-build/bin
