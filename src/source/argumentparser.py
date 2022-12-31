4e3622cbb7a0fa2e8c723c20983bafaf298759cd
policere <59689715+policere@users.noreply.github.com>
2022-12-31 14:52:54 +0200

    ArgumentParser subcommands, reply-to argument, caching, fixes, new features

--- src/source/argumentparser.py
+++ src/source/argumentparser.py
@@ -11,11 +11,17 @@ escape_dict = {
     "'": "'"
 }
 
+# number of messages to iter when reply-to functional is used
+RT_ITER_NUM = 50
+
 
 class ArgumentParseError(Enum):
     TooManyArguments = auto()
     TooLittleArguments = auto()
     IncorrectType = auto()
+    IncorrectSubcommand = auto()
+    ReplyToRequired = auto()
+    CantFindOriginalMessage = auto()
 
 
 class ArgTypeCast:
@@ -249,19 +255,76 @@ class Argument:
             return True, e
 
 
-class ArgumentParser:
-    """Helps to manage the arguments"""
+class ReplyToArgument(Argument):
+    """Argument for the Reply-To functional"""
 
-    def __init__(self, parent_plugin, arguments: list[Argument]):
+    def __init__(self,
+                 arg_name: str,
+                 arg_type: ArgTypeCast = Cast.StrCast,
+                 default: Any | None = None,
+                 description: str = 'Sample argument'
+                 ):
+        """
+        :param arg_name: Name of the argument
+        :type arg_name: str
+        :param arg_type: Type of the argument (NOTE: It must support type-casting with using str)
+        :type arg_type: type
+        :param default: Defines default value of the argument. If it None - argument is marked as required
+        :type default: Any | None
+        :param description: Description of the argument
+        :type description: str
+        """
+        super().__init__(arg_name, arg_type, default, description)
+
+
+# cache of the ArgumentParser
+_CACHED = {}
+
+
+class ArgumentParser:
+    """Helps to manage the arguments
+    
+    :ivar parent_plugin: Parent plugin of this parser
+    :type parent_plugin: Plugin
+    :ivar arguments: List with the arguments
+    :type arguments: list[Argument]
+    :ivar allow_caching: Use the caching when it parses the arguments
+    :type allow_caching: bool
+    :ivar logger: Logger of the argument parser
+    :type logger: logging.Logger
+    :ivar position_arguments: Number of the position arguments
+    :type position_arguments: int
+    :ivar default_arguments: Number of the default arguments
+    :type default_arguments: int
+    """
+
+    def __init__(self, parent_plugin,
+                 arguments: list[Argument],
+                 allow_caching: bool = True,
+                 enable_escaping: bool = False,
+                 subcommands: bool = False,
+                 main_command_aliases: str | list[str] | None = None
+                 ):
         """
         :param parent_plugin: Parent plugin of this parser
         :type parent_plugin: Plugin
         :param arguments: List with the arguments
         :type arguments: list[Argument]
+        :param allow_caching: Use the caching when it parses the arguments
+        :type allow_caching: bool
+        :param enable_escaping: Enable the escaping by "\" character
+        :type enable_escaping: bool
+        :param subcommands: Enable the subcommands feature
+        :type subcommands: bool
+        :param main_command_aliases: Name of the main commands (requires "subcommands")
+        :type main_command_aliases: str | list[str] | None = None
         """
 
         self.parent_plugin = parent_plugin
         self.arguments = arguments
+        self.allow_caching = allow_caching
+        self.enable_escaping = enable_escaping
+        self.subcommands = subcommands
 
         self.logger = logging.getLogger('EasyTl : ArgumentParser')
 
@@ -276,23 +339,48 @@ class ArgumentParser:
 
             self.position_arguments += 1
 
-    async def parse(self, args: list[str]) -> (bool, Namespace | ArgumentParseError):
-        """(System) Parses the default EasyTl arguments system to new
+        self.subcommands_dict = {}
 
-        :param args: List with the arguments
-        :type args: list[str]
+        if self.subcommands:
+            # create empty command by the plugin decorator
+            self.parent_plugin.command(main_command_aliases, ap=self)(self.parent_plugin.async_empty)
 
-        :return: Namespace with the parsed arguments
-        :rtype: (bool, Namespace | ArgumentParseError)
+        if len(self.arguments) > 0 and isinstance(self.arguments[0], ReplyToArgument):
+            if self.arguments[0].default is None:
+                self.position_arguments -= 1
+
+    def _cached_check(self, string: str) -> list[str]:
+        """Checker for the caching
+
+        :param string: Result work or cached string
+        :type string: str
+
+        :return: Executed cached result or already cached result of the parse_string() method
+        :rtype: list[str]
         """
 
-        self.logger.debug('ArgumentParser called with arguments list: ' + ', '.join(args))
+        if not self.allow_caching:
+            return self.parse_string(string)
 
-        # initialize the variables
-        output_args = Namespace()
-        output_args.PREFIX = args[0]
-        output_args.CMD = args[1]
-        input_str = ' '.join(args[2:]) + '\00' if len(args) > 2 else ''
+        # get hash of the string
+        str_hash = hash(string)
+
+        if str_hash not in _CACHED:
+            _CACHED[str_hash] = self.parse_string(string)
+
+        return _CACHED[str_hash]
+
+    ####
+
+    def parse_string(self, string: str) -> list[str]:
+        """Parses the string to the list with the arguments
+
+        :param string: String to parse
+        :type string: str
+        
+        :return: List with the parsed arguments
+        :rtype: list[str]
+        """
 
         # initialize the variables to first parse stage
         str_open = False
@@ -302,12 +390,14 @@ class ArgumentParser:
         escaped = False
 
         # first parse stage (strings with spaces, escaping)
-        for s in input_str:
+        for s in string:
 
             if escaped:
                 if s not in escape_dict:
+                    # do not escape the character
                     temp_str += '\\' + s
                 else:
+                    # escape the character to other character in escape dict
                     temp_str += escape_dict[s]
 
                 escaped = False
@@ -315,13 +405,19 @@ class ArgumentParser:
 
             match s:
                 case '\\':
+                    # check if escaping is enabled
+                    if not self.enable_escaping:
+                        continue
+
                     escaped = True
 
                 case "'" | '"':
+                    # check if characters is different
                     if str_open_char is not None and str_open_char != s:
                         temp_str += s
                         continue
 
+                    # check for the opened string
                     if str_open:
                         str_open = False
                         str_open_char = None
@@ -339,20 +435,103 @@ class ArgumentParser:
                         temp_str += ' '
 
                 case '\00':
-                    temp_args.append(temp_str)
+                    if temp_str != '':
+                        temp_args.append(temp_str)
 
                 case _:
                     temp_str += s
 
+        return temp_args
+
+    async def parse(self, args: list[str], event, command_func) -> (bool, Namespace | ArgumentParseError):
+        """Parses the default EasyTl arguments system to new & execute the command
+
+        :param args: List with the arguments
+        :type args: list[str]
+        :param event: Telethon event
+        :param command_func: Command function
+        :type command_func: (Any, Namespace) -> None
+
+        :return: Namespace with the parsed arguments
+        :rtype: (bool, Namespace | ArgumentParseError)
+        """
+
+        self.logger.debug('ArgumentParser called with arguments: ' + ', '.join(args))
+
+        # check for the subcommands
+        if self.subcommands:
+            self.logger.debug('Command with subcommands has called')
+
+            # check for the arguments length
+            if len(args) < 2:
+                self.logger.debug('Too little arguments')
+                return ArgumentParseError.TooLittleArguments
+
+            # check for the subcommand
+            if args[2] in self.subcommands_dict:
+                self.logger.debug('Call the subcommand function')
+                func = self.subcommands_dict[args[2]]  # get function
+
+                # check for the ArgumentParser is using in command
+                if func.ap is not None:
+                    return await func.ap.parse([args[0]] + args[2:], event, func)
+
+                self.logger.info(f'Function {func.__name__} (command: {args[2]}) doesn\'t have ArgumentParser!')
+                return None
+            else:
+                return ArgumentParseError.IncorrectSubcommand
+
+        # initialize the variables
+        output_args = Namespace()
+        output_args.PREFIX = args[0]
+        output_args.CMD = args[1]
+        temp_args = []
+
+        if len(args) > 2:
+            input_str = ' '.join(args[2:]) + '\00'
+
+            # parse the stringuity
+            temp_args = self._cached_check(input_str)
+
+        # check for the reply-to argument
+        if len(self.arguments) > 0 and isinstance(self.arguments[0], ReplyToArgument):
+            arg = self.arguments[0]
+
+            # check if message has reply-to
+            if event.reply_to:
+                # find reply-to message
+                msg = [msg async for msg in self.parent_plugin.namespace.instance.client.iter_messages(event.chat_id, RT_ITER_NUM)
+                       if msg.id == event.reply_to.reply_to_msg_id]
+                if len(msg) == 0:
+                    return ArgumentParseError.CantFindOriginalMessage
+                msg = msg[0]
+
+                # type-cast the argument
+                error, result = arg.typecast(msg.message)
+
+                # check for the error
+                if error:
+                    self.logger.debug('can\'t cast the argument')
+                    log_exception(self.logger, result)
+
+                    return ArgumentParseError.IncorrectType
+
+                # set up the type-casted reply-to message
+                setattr(output_args, arg.arg_name, result)
+                setattr(output_args, 'REPLY_TO_MESSAGE', msg)
+            else:
+                if arg.default is None:
+                    return ArgumentParseError.ReplyToRequired
+
         # compare the positional arguments
         if len(temp_args) < self.position_arguments:
             self.logger.debug('Too little arguments')
-            return True, ArgumentParseError.TooLittleArguments
+            return ArgumentParseError.TooLittleArguments
 
         # compare the arguments sum length
         if len(temp_args) > self.position_arguments + self.default_arguments:
             self.logger.debug('Too much arguments')
-            return True, ArgumentParseError.TooManyArguments
+            return ArgumentParseError.TooManyArguments
 
         # initialize the default arguments
         for arg in self.arguments:
@@ -369,9 +548,31 @@ class ArgumentParser:
                 self.logger.debug('can\'t cast the argument')
                 log_exception(self.logger, result)
 
-                return True, ArgumentParseError.IncorrectType
+                return ArgumentParseError.IncorrectType
 
             # define variable
             setattr(output_args, arg.arg_name, result)
 
-        return False, output_args
+        # call the command
+        await self.parent_plugin.namespace.call_w_permissions(command_func, event, output_args)
+
+        return None
+
+    ####
+
+    def subcommand(self, aliases: str | list | None = None, ap=None, static_pname: str | None = None):
+
+        def deco(func):
+            nonlocal aliases
+
+            # register command by the plugins decorator
+            func = self.parent_plugin.command(aliases, ap, static_pname)(func)
+
+            # register aliases in the argument parser subcommands dict
+            aliases = aliases if isinstance(aliases, list) else [aliases, ]
+            for a in aliases:
+                self.subcommands_dict[a] = func
+
+            return func
+
+        return deco
